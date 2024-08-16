001f21ca6d899c88b2e384c82646a23b6a392cac
garvit <garvitpathak2003@gmail.com>
2024-08-17 01:59:26 +0530

    edited yaml file

--- node_app/terraform/variable.tf
+++ node_app/terraform/variable.tf
@@ -4,16 +4,13 @@ variable "region" {
 }
 
 variable "public_key" {
-  description = "The public key for SSH access"
-  type        = string
+  
 }
 
 variable "private_key" {
-  description = "The private key for SSH access"
-  type        = string
+  
 }
 
 variable "key_name" {
-  description = "The name of the key pair"
-  type        = string
+  
 }
