35a11213fd334d818ba4ff3b114f39c3eaf0ccb5
garvit <garvitpathak2003@gmail.com>
2024-08-17 19:42:01 +0530

    error might be in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "servernode" {
   ami                    = "ami-04a81a99f5ec58529"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
@@ -34,47 +34,49 @@ resource "aws_instance" "server" {
   }
 }
 
-resource "aws_iam_instance_profile" "ec2profile" {
+resource "aws_iam_instance_profile" "ec2-profile" {
   name = "ec2-profile"
   role = "EC2-EC3-AUTH"
 }
 
 resource "aws_security_group" "maingroup" {
-  egress {
-    cidr_blocks       = ["0.0.0.0/0"]
-    description       = ""
-    from_port         = 0
-    ipv6_cidr_blocks  = []
-    prefix_list_ids   = []
-    protocol          = "-1"
-    security_groups   = []
-    self              = false
-    to_port           = 0
-  }
-
-  ingress {
-    cidr_blocks       = ["0.0.0.0/0"]
-    description       = ""
-    from_port         = 22
-    ipv6_cidr_blocks  = []
-    prefix_list_ids   = []
-    protocol          = "tcp"
-    security_groups   = []
-    self              = false
-    to_port           = 22
-  }
-
-  ingress {
-    cidr_blocks       = ["0.0.0.0/0"]
-    description       = ""
-    from_port         = 80
-    ipv6_cidr_blocks  = []
-    prefix_list_ids   = []
-    protocol          = "tcp"
-    security_groups   = []
-    self              = false
-    to_port           = 80
-  }
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
 }
 
 resource "aws_key_pair" "deployer" {
