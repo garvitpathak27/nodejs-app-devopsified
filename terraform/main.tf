ec5d4e121496b1a1ad6e85b7be2647c84f31f8c3
garvit <garvitpathak2003@gmail.com>
2024-08-17 19:43:14 +0530

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "servernode" {
+resource "aws_instance" "server" {
   ami                    = "ami-04a81a99f5ec58529"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
